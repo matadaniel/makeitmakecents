641106706152dc4d8144f243a04098e46119c04e
Daniel Mata <100956688+matadaniel@users.noreply.github.com>
2023-01-09 21:32:07 -0600

    Install Skeleton

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script>
+	import '@skeletonlabs/skeleton/themes/theme-seafoam.css';
+	import '@skeletonlabs/skeleton/styles/all.css';
+	import '../app.postcss';
+
+	import { AppShell } from '@skeletonlabs/skeleton';
+</script>
+
+<AppShell><slot /></AppShell>
