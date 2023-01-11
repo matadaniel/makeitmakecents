3418040d2d6954ac88b2a1409af15a9d67fea9e6
Daniel Mata <100956688+matadaniel@users.noreply.github.com>
2023-01-10 22:32:58 -0600

    Add page content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,19 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	let expenses: number;
+
+	const handleSubmit = () => {};
+</script>
+
+<h1>Congratulations!!</h1>
+<h2>An oil prince has just deposited $100,000 into your bank account!</h2>
+<p>Are you able to sleep in tomorrow? And the day after that, and the day after that,</p>
+
+<br />
+
+<form on:submit|preventDefault={handleSubmit}>
+	<label for="expenses">
+		<span>Yearly Expenses:</span>
+		<input type="number" id="expenses" bind:value={expenses} required />
+	</label>
+	<button class="btn btn-filled-primary">Submit</button>
+</form>
