96ecc64e1ad6d75dda19e89c0b3883cd71d8600e
Daniel Mata <100956688+matadaniel@users.noreply.github.com>
2023-01-12 23:14:30 -0600

    Create NumInput
    specified type attribute for two-way component binding

---
+++ src/lib/NumInput.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import type { HTMLInputAttributes } from 'svelte/elements';
+
+	export let inputAttributes: HTMLInputAttributes & { id: string };
+	export let labelText: string;
+	export let value: any = undefined;
+</script>
+
+<label for={inputAttributes.id}>
+	<span>{labelText}</span>
+	<input type="number" bind:value {...inputAttributes} />
+</label>
