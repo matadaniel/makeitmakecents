6a87d42609cc1fb102b394b81aa75cf984d942f7
Daniel Mata <100956688+matadaniel@users.noreply.github.com>
2023-01-23 17:12:35 -0600

    Add more info

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,7 @@
 	import { currency } from '$lib/stores';
 	import { reporter } from '@felte/reporter-svelte';
 	import { validator } from '@felte/validator-zod';
+	import { tooltip } from '@skeletonlabs/skeleton';
 	import { createForm } from 'felte';
 	import { z } from 'zod';
 
@@ -17,8 +18,6 @@
 
 	const { form } = createForm<z.infer<typeof schema>>({
 		onSubmit: ({ expenses, savings }) => {
-			// https://ssrn.com/abstract=3964908
-
 			expenses /= 12;
 
 			const stockReturns = 0.0075;
@@ -66,7 +65,10 @@
 
 <h1>Congratulations!!</h1>
 <h2>An oil prince has just deposited $100,000 into your bank account!</h2>
-<p>Are you able to sleep in tomorrow? And the day after that, and the day after that,</p>
+<p>
+	Are you able to sleep in tomorrow? And the day after that, and the day after that, and the day
+	after that...
+</p>
 
 <br />
 
@@ -94,7 +96,43 @@
 	<button class="btn btn-filled-primary">Submit</button>
 </form>
 
-{#key data}{#if data}<AreaChart {data} />{/if}{/key}
+<div class="card flex flex-col items-center my-16">
+	{#key data}
+		{#if data}
+			{@const months = data[0].data.length - 1}
+
+			<header class="card-header">
+				<p class="unstyled text-center text-3xl font-medium">
+					Your money should <a href="https://ssrn.com/abstract=3964908"
+						><i
+							use:tooltip={{
+								content: `<p>60% stocks / 40% bonds</p>
+<p>2% inflation</p>
+<p>
+	Average nominal return data:<br />
+	https://ssrn.com/abstract=3964908
+</p>`,
+								width: '!w-72'
+							}}>theoretically</i
+						></a
+					><br />last you {#if months > 11}{`${Math.trunc(months / 12)} year${
+							months > 23 ? 's' : ''
+						} and `}{/if}{`${months % 12} ${
+						months % 12 === 1 ? 'month' : 'months'
+					}`}{#if months === 81 * 12}{' or more'}{/if}.
+				</p>
+			</header>
+
+			<AreaChart {data} />
+
+			<footer class="card-footer">
+				<small>Extrapolated from historical data.</small>
+			</footer>
+		{:else}
+			<div class="placeholder w-[600px] h-[503px] animate-pulse" />
+		{/if}
+	{/key}
+</div>
 
 <p>
 	Investing always carries some level of risk. The past performance of any investment is not a
