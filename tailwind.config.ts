d9798595653d1894b3be587d4056e9e46a1c8cb0
Daniel Mata <100956688+matadaniel@users.noreply.github.com>
2024-03-14 15:30:33 -0500

    Move Tailwind config and themes

---
+++ tailwind.config.ts
@@ -0,0 +1,16 @@
+import { skeleton } from '@skeletonlabs/tw-plugin';
+import forms from '@tailwindcss/forms';
+import { join } from 'path';
+import type { Config } from 'tailwindcss';
+
+export default {
+	darkMode: 'class',
+	content: [
+		'./src/**/*.{html,js,svelte,ts}',
+		join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')
+	],
+	theme: {
+		extend: {}
+	},
+	plugins: [forms, skeleton({ themes: { preset: ['seafoam'] } })]
+} satisfies Config;
