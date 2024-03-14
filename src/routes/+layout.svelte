bbcd68b0d8009922624e6dd6c8f673e56de68544
Daniel Mata <100956688+matadaniel@users.noreply.github.com>
2024-03-13 22:42:19 -0500

    Remove Skeleton stylesheet imports

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,4 @@
 <script>
-	import '@skeletonlabs/skeleton/themes/theme-seafoam.css';
-	import '@skeletonlabs/skeleton/styles/all.css';
 	import '../app.postcss';
 
 	import { AppShell } from '@skeletonlabs/skeleton';
