ba17c9ee0dbb6af83baa0cd78022232b93aaac4c
Daniel Mata <100956688+matadaniel@users.noreply.github.com>
2024-05-11 20:10:21 -0500

    Add formatToCurrency derived store

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -1,3 +1,11 @@
-import { writable } from 'svelte/store';
+import { derived, writable } from 'svelte/store';
 
 export const currency = writable('USD');
+
+export const locale = writable(undefined);
+
+export const formatToCurrency = derived(
+	[currency, locale],
+	([$currency, $locale]) =>
+		new Intl.NumberFormat($locale, { style: 'currency', currency: $currency }).format
+);
