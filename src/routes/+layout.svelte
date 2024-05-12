f6ede3eb9cb5769b96965dbec400d46d7efdaa9d
Daniel Mata <100956688+matadaniel@users.noreply.github.com>
2024-05-11 20:14:16 -0500

    Add min-h-screen class to slotPageContent

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,7 +8,10 @@
 
 <Modal />
 
-<AppShell slotPageContent="flex flex-col items-center mt-24" slotPageFooter="mt-24 mx-3 mb-3">
+<AppShell
+	slotPageContent="flex flex-col items-center mt-24 min-h-screen"
+	slotPageFooter="mt-24 mx-3 mb-3"
+>
 	<slot />
 
 	<svelte:fragment slot="pageFooter">
