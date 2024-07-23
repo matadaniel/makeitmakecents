2497e224cbce70587cbc332f7843320a725ba361
Daniel Mata <100956688+matadaniel@users.noreply.github.com>
2024-07-23 13:06:39 -0500

    Create Navigation

---
+++ src/lib/Navigation/Navigation.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import { getDrawerStore } from '@skeletonlabs/skeleton';
+
+	const drawerStore = getDrawerStore();
+
+	function drawerClose(): void {
+		drawerStore.close();
+	}
+
+	const links = [
+		['/waiting-cost', 'The cost of waiting'],
+		['/', 'Lump Sum']
+	];
+
+	const modules = import.meta.glob('/src/routes/**/+page.svelte');
+	const routes = Object.keys(modules).map((k) => k.substring(11, k.length - 13) || '/');
+	if (routes.length !== links.length) throw new Error('routes length does not match links length');
+	const hrefs = new Set<string>();
+	for (const [href] of links) hrefs.add(href);
+	for (const route of routes) if (!hrefs.has(route)) throw new Error(`${route} not in links`);
+</script>
+
+<nav class="list-nav p-4">
+	<ul>
+		{#each links as [href, title]}
+			<li><a {href} on:click={drawerClose}>{title}</a></li>
+		{/each}
+	</ul>
+</nav>
